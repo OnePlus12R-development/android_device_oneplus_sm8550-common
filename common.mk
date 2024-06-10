197849213bdb46937c1b6d3a487923d611dee31b
Sugakesshaa <sugakesshaa@gmail.com>
2024-06-10 19:54:14 +0530

    sm8550-common: Import powerhint.json from taro from https://github.com/PixelOS-Devices/device_xiaomi_sm8450-common/blob/fourteen/configs/powerhint.json
    Signed-off-by: inferno0230 <mail@inferno0230.in>

--- common.mk
+++ common.mk
@@ -274,6 +274,9 @@ PRODUCT_PACKAGES += \
     android.hardware.power-service.lineage-libperfmgr \
     libqti-perfd-client
 
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json
+
 # QSPA
 PRODUCT_PACKAGES += \
     vendor.qti.qspa-service
