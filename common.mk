0295fb7d8e2f7c0ddb09d9c4d93ebe7f57feb441
Sugakesshaa <sugakesshaa@pixelos.net>
2024-06-10 19:43:45 +0530

    sm8550-common: Move init.qcom.post_boot.sh to tree
    Signed-off-by: inferno0230 <mail@inferno0230.in>

--- common.mk
+++ common.mk
@@ -256,6 +256,7 @@ PRODUCT_PACKAGES += \
     init.oplus.hw.rc.recovery \
     init.oplus.rc \
     init.qcom.early_boot.sh \
+    init.qcom.post_boot.sh \
     init.qcom.rc \
     init.qcom.recovery.rc \
     init.qcom.sh \
