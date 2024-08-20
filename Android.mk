be0f08caf48f2ab57e588c4d73bfa679969bf133
inferno0230 <mail@inferno0230.in>
2024-08-20 16:50:59 +0530

    sm8550-common: Add xueying to build guards
    Signed-off-by: inferno0230 <mail@inferno0230.in>

--- Android.mk
+++ Android.mk
@@ -6,7 +6,7 @@
 
 LOCAL_PATH := $(call my-dir)
 
-ifneq ($(filter salami aston,$(TARGET_DEVICE)),)
+ifneq ($(filter salami aston xueying,$(TARGET_DEVICE)),)
 include $(call all-makefiles-under,$(LOCAL_PATH))
 
 include $(CLEAR_VARS)
