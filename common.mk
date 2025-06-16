e7a59308aa18b2d877a9e182f53aa0a39a3fa637
basamaryan <basam.aryan@gmail.com>
2025-06-16 11:19:48 -0400

    sm8550-common: Drop legacy ANT remnants
    QSSI only has platform/external/ant-wireless/hidl since a while
    
    Change-Id: Ibccfdc7e72efd3c9d3ff62bda3e729b99433a567

--- common.mk
+++ common.mk
@@ -26,10 +26,6 @@ PRODUCT_PACKAGES += \
     checkpoint_gc \
     otapreopt_script
 
-# ANT+
-PRODUCT_PACKAGES += \
-    AntHalService-Soong
-
 ifeq ($(TARGET_DEVICE),benz)
 # Alert slider
 PRODUCT_PACKAGES += \
