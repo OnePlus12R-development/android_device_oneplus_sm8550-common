a5997572ab561852a9bb96c020f45ae1ae17d330
inferno0230 <mail@inferno0230.in>
2026-01-30 14:55:14 +0530

    Revert "sm8550-common: Patch some blobs to depend on libtinyxml2-v34.so"
    This reverts commit d5a61528a1058ff084646eb95ce0329f04f3f689.

--- extract-files.py
+++ extract-files.py
@@ -67,21 +67,6 @@ lib_fixups: lib_fixups_user_type = {
 }
 
 blob_fixups: blob_fixups_user_type = {
-    (
-        'odm/bin/touchDaemon',
-        'odm/bin/hw/vendor.oplus.hardware.biometrics.fingerprint@2.1-service_uff',
-        'vendor/bin/poweropt-service',
-        'vendor/lib64/libaodoptfeature.so',
-        'vendor/lib64/libapengine.so',
-        'vendor/lib64/libdpps.so',
-        'vendor/lib64/liblearningmodule.so',
-        'vendor/lib64/libpowercore.so',
-        'vendor/lib64/libpsmoptfeature.so',
-        'vendor/lib64/libsnapdragoncolor-manager.so',
-        'vendor/lib64/libstandbyfeature.so',
-        'vendor/lib64/libvideooptfeature.so',
-    ): blob_fixup()
-        .replace_needed('libtinyxml2.so', 'libtinyxml2-v34.so'),
     'odm/bin/hw/vendor.oplus.hardware.biometrics.fingerprint@2.1-service_uff': blob_fixup()
         .add_needed('libshims_aidl_fingerprint_v2.oplus.so'),
     'odm/bin/hw/vendor.oplus.hardware.charger-V9-service': blob_fixup()
